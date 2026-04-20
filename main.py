6bfca683e4fbed18f44566cae5d7c0b4c82ff545
Simón Acosta <simonacostfc@gmail.com>
2026-04-20 16:24:58 -0500

    [Update] crear estudiante main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, HTTPException
+from Estudiante_operations import createEstudiante
+from models import (EstudianteBase, EstudianteId)
 
 app = FastAPI()
 
 @app.get("/hola")
 def hola():
     return {"message": "Hello World"}
+
+@app.post("/pokemon", response_model=EstudianteId)
+async def create_pokemon(pokemon:EstudianteBase):
+    return createEstudiante(pokemon)
