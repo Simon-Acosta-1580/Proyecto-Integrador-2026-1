d3b137a1b48f1bb2eb61566cd4be5c0b5fc646f3
Simón Acosta <simonacostfc@gmail.com>
2026-05-05 08:28:53 -0500

    [UPDATE] mostrar estudiantes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from operations.Estudiante_operations import createEstudiante
+from operations.Estudiante_operations import createEstudiante, show_all_estudiantes
 from operations.Implemento_operations import createImplemento, showImplementos, showImplementosInactivos, showImplemento, getImplementoByCategoria, deleteImplemento, updateImplemento
 from operations.turno_Operations import createTurno, showTurnos, showTurno, showTurnosInactivos, getTurnosByHorario, deleteTurno, updateTurno
 from models import EstudianteBase, EstudianteId, EstudianteUpdate, ImplementoBase, ImplementoId, TurnoBase, TurnoId
@@ -14,8 +14,15 @@ def hola():
 
 @app.post("/estudiante", response_model=EstudianteId, tags=["Estudiantes"])
 async def create_estudiante(estudiante: EstudianteBase, session: SessionDep):
-    return createEstudiante(estudiante, session)
+    new_estudiante=createEstudiante(estudiante, session)
+    if not new_estudiante:
+        raise HTTPException(status_code=404, detail=f"El estudiante con código {estudiante.codigo} ya existe.")
 
-
+@app.get("/estudiantes", response_model=list[EstudianteId], tags=["Estudiantes"])
+async def read_estudiantes(session: SessionDep):
+    lista_estudiantes = show_all_estudiantes(session)
+    if not lista_estudiantes:
+        raise HTTPException(status_code=404, detail=f"No se encontraron estudiantes registrados")
+    return lista_estudiantes
 
 
--- operations/Estudiante_operations.py
+++ operations/Estudiante_operations.py
@@ -3,9 +3,16 @@ from sqlmodel import Session, select
 from models import EstudianteBase, EstudianteId
 
 def createEstudiante(estudiante: EstudianteBase, session: Session):
+    lista_estudiantes = show_all_estudiantes(session)
+    for e in lista_estudiantes:
+        if e.codigo == estudiante.codigo:
+            return None
     new_estudiante = EstudianteId.model_validate(estudiante)
     session.add(new_estudiante)
     session.commit()
     session.refresh(new_estudiante)
 
     return new_estudiante
+
+def show_all_estudiantes(session: Session):
+    return session.exec(select(EstudianteId))
