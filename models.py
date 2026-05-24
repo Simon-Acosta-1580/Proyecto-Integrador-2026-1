c304cf5ca1c9e30d76c99baf2fa086184e9255c7
Simón Acosta <simonacostfc@gmail.com>
2026-05-23 19:38:44 -0500

    [UPDATE] relacion dependencia turno con estudiantes y implementos

--- models.py
+++ models.py
@@ -37,6 +37,8 @@ class ImplementoUpdate(ImplementoBase):
     activo: bool = Field(None, exclude=True)
 
 class TurnoBase(SQLModel):
+    estudiante_id: int = Field(foreign_key="estudianteid.id")
+    implementor_id: int = Field(foreign_key="implementorid.id")
     codigo: int = Field(..., gt=0)
     hora_inicio: int = Field(..., gt=8, le=19)
     horario: Horario = Field(..., min_length=1, max_length=50)
@@ -51,5 +53,20 @@ class TurnoBase(SQLModel):
 
         return self
 
-class TurnoId(TurnoBase):
-    id: int = Field(..., gt=0)
+class TurnoId(TurnoBase, table=True):
+    id: int = Field(default=None, primary_key=True, gt=0)
+
+class turnoUpdate(TurnoBase):
+    codigo: int = Field(None, exclude=True)
+    hora_inicio: int = Field(..., gt=8, le=19)
+    horario: Horario = Field(..., min_length=1, max_length=50)
+    activo: bool = Field(None, exclude=True)
+
+    @model_validator(mode='after')
+    def validar_horario_diurno(self):
+        es_diurno = self.horario.lower() == "diurno"
+
+        if es_diurno and self.hora_inicio >= 18:
+            raise ValueError("Los turnos de tipo 'Diurno' no pueden iniciar después de las 18 horas.")
+
+        return self
